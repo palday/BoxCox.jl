c9dd72a3a1d80faa22bd8bd25b835e5eeb38ef2d
Phillip Alday <me@phillipalday.com>
2024-08-07 22:20:21 -0500

    refactor to make adding additional transformations a little bit easier

--- ext/BoxCoxMixedModelsExt.jl
+++ ext/BoxCoxMixedModelsExt.jl
@@ -6,54 +6,25 @@ using MixedModels
 using BoxCox: StatsAPI, NLopt
 using BoxCox: _boxcox!, geomean, chisqinvcdf,
               _loglikelihood_boxcox!,
-              _loglikelihood_boxcox
+              _loglikelihood_boxcox,
+              _llfunc, _llfunc!,
+              _input_check,
+              _scaling,
+              PowerTransformation
 using MixedModels: refit!
 
-function StatsAPI.fit(::Type{BoxCoxTransformation}, model::LinearMixedModel; progress=true,
-                      algorithm::Symbol=:LN_BOBYQA, opt_atol=1e-8, opt_rtol=1e-8,
-                      maxiter=-1, kwargs...)
-    isfitted(model) ||
-        throw(ArgumentError("Expected model to be fitted, but `isfitted(model)` is false."))
-    y = response(model)
-    any(<=(0), y) && throw(ArgumentError("all y values must be greater than zero"))
-    y = float.(y)  # we modify, so let's make a copy!
-    y ./= geomean(y)
-    model = deepcopy(model)
-    opt = NLopt.Opt(algorithm, 1)
-    NLopt.xtol_abs!(opt, opt_atol) # relative criterion on parameter values
-    NLopt.xtol_rel!(opt, opt_rtol) # relative criterion on parameter values
-    NLopt.maxeval!(opt, maxiter)
-    local y_trans = similar(y)
-    function obj(λvec, g)
-        isempty(g) || throw(ArgumentError("g should be empty for this objective"))
-        val = _loglikelihood_boxcox!(y_trans, model, y, only(λvec); progress)
-        return val
-    end
-    opt.max_objective = obj
-    (ll, λ, retval) = NLopt.optimize(opt, [0.0])
-
-    return BoxCoxTransformation{LinearMixedModel}(; λ=only(λ), y, X=model, kwargs...)
-end
-
-function BoxCox._loglikelihood_boxcox!(y_trans::Vector{<:Number}, model::LinearMixedModel,
-                                       y::Vector{<:Number}, λ::Number;
-                                       progress=true, kwargs...)
-    _boxcox!(y_trans, y, λ; kwargs...)
-    refit!(model, y_trans; progress)
-    y_trans .-= fitted(model)
-    return _loglikelihood_boxcox(y_trans)
-end
-
-StatsAPI.nobs(bc::BoxCoxTransformation{LinearMixedModel}) = nobs(bc.X)
-
-function StatsAPI.confint(bc::BoxCoxTransformation{LinearMixedModel}; level::Real=0.95,
-                          fast::Bool=nobs(bc) > 10_000, progress=true)
+# TODO: jiggle types slightly so that this works with any powertransformation
+# (maybe make PowerTransformation parametric?)
+function StatsAPI.confint(bc::T; level::Real=0.95,
+                          fast::Bool=nobs(bc) > 10_000, progress=true) where
+                          {T<:BoxCoxTransformation{LinearMixedModel}}
     lltarget = loglikelihood(bc) - chisqinvcdf(1, level) / 2
     opt = NLopt.Opt(:LN_BOBYQA, 1)
     y_trans = similar(bc.y)
+    ll! = _llfunc!(T)
     function obj(λvec, g)
         isempty(g) || throw(ArgumentError("g should be empty for this objective"))
-        llhat = _loglikelihood_boxcox!(y_trans, bc.X, bc.y, only(λvec); progress)
+        llhat = ll!(y_trans, bc.X, bc.y, only(λvec); progress)
         # want this to be zero
         val = abs(llhat - lltarget)
         return val
@@ -75,6 +46,46 @@ function StatsAPI.confint(bc::BoxCoxTransformation{LinearMixedModel}; level::Rea
     return [lower, upper]
 end
 
+function StatsAPI.fit(T::Type{<:PowerTransformation}, model::LinearMixedModel; progress=true,
+                      algorithm::Symbol=:LN_BOBYQA, opt_atol=1e-8, opt_rtol=1e-8,
+                      maxiter=-1, kwargs...)
+    isfitted(model) ||
+        throw(ArgumentError("Expected model to be fitted, but `isfitted(model)` is false."))
+    y = response(model)
+    _input_check(T)(y)
+    y = float.(y)  # we modify, so let's make a copy!
+    y ./= _scaling(T)(y)
+    model = deepcopy(model)
+    opt = NLopt.Opt(algorithm, 1)
+    NLopt.xtol_abs!(opt, opt_atol) # relative criterion on parameter values
+    NLopt.xtol_rel!(opt, opt_rtol) # relative criterion on parameter values
+    NLopt.maxeval!(opt, maxiter)
+    local y_trans = similar(y)
+    ll! = _llfunc!(T)
+    function obj(λvec, g)
+        isempty(g) || throw(ArgumentError("g should be empty for this objective"))
+        val = ll!(y_trans, model, y, only(λvec); progress)
+        return val
+    end
+    opt.max_objective = obj
+    (ll, λ, retval) = NLopt.optimize(opt, [0.0])
+
+    return T{LinearMixedModel}(; λ=only(λ), y, X=model, kwargs...)
+end
+
+#####
+##### Box Cox
+#####
+
+function BoxCox._loglikelihood_boxcox!(y_trans::Vector{<:Number}, model::LinearMixedModel,
+                                       y::Vector{<:Number}, λ::Number;
+                                       progress=true, kwargs...)
+    _boxcox!(y_trans, y, λ; kwargs...)
+    refit!(model, y_trans; progress)
+    y_trans .-= fitted(model)
+    return _loglikelihood_boxcox(y_trans)
+end
+
 function BoxCox._loglikelihood_boxcox(λ::Number, model::LinearMixedModel,
                                       y::Vector{<:Number};
                                       kwargs...)
@@ -90,4 +101,5 @@ function BoxCox._loglikelihood_boxcox(model::LinearMixedModel, y::Vector{<:Numbe
     end
     return ll
 end
+
 end # module
